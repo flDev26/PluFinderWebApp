3e5d9e0696da0c28aec0e966c01584652e32d131
FlavioCPT <cpcpalacios12@gmail.com>
2024-07-15 18:40:59 -0500

    DBconnected_modelTweaked_bizLogicSet_inProgress

--- src/main/java/model/Products.java
+++ src/main/java/model/Products.java
@@ -1,4 +1,4 @@
-package model;
+package Model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,27 +8,27 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table (name="products_inDatabase")
+@Table (name="products_indatabase")
 public class Products{
 	//MAPPED CLASS VARIABLES
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	@Column(name="name_inDB")
+	@Column(name="name_in_db")
 	private String productName;
-	@Column(name="mainImage_inDB")
+	@Column(name="mainImage_in_db")
     private String mainImageUrl;
-	@Column(name="price_inDB")
+	@Column(name="price_in_db")
     private long priceInCents;
-	@Column(name="unit_inDB")
+	@Column(name="unit_in_db")
     private String unit;
-	@Column(name="plu_inDB")
+	@Column(name="plu_in_db")
 	private int plu;
-	@Column(name="department_inDB")
+	@Column(name="department_in_db")
 	private String department;
-	@Column(name="category_inDB")
+	@Column(name="category_in_db")
     private String category;
-	@Column(name="description_inDB")
+	@Column(name="description_in_db")
     private String description;
     
     //CLASS CONSTRUCTORS
